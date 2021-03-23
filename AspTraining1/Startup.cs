724989462bdaae8581a067eb18037da94913805e
Ryan Elian <ryan.elian.cs@gmail.com>
2021-03-24 05:32:06 +0700

    Enable Web API

--- AspTraining1/Startup.cs
+++ AspTraining1/Startup.cs
@@ -26,6 +26,7 @@ namespace AspTraining1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
@@ -52,6 +53,7 @@ namespace AspTraining1
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapDefaultControllerRoute();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
