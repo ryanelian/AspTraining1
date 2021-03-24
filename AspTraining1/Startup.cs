2b70082a0a0cddbd90f47e5a79a545d34c72aee4
Ryan Elian <ryan.elian.cs@gmail.com>
2021-03-24 22:55:14 +0700

    Disable HTTPS

--- AspTraining1/Startup.cs
+++ AspTraining1/Startup.cs
@@ -66,11 +66,8 @@ namespace AspTraining1
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseOpenApi();
